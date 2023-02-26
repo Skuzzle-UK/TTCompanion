ade8e5691375a308dc494ea927f12336efbe2d5d
Nick Bailey <nick@skuzzle.com>
2023-02-26 21:35:15 +0000

    Added template tests project for API

---
+++ TTCompanion.API.Tests/SampleTest.cs
@@ -0,0 +1,11 @@
+namespace TTCompanion.API.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void SampleTest1()
+        {
+            Assert.True(true);
+        }
+    }
+}
